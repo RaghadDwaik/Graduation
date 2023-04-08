98987e94a8026d70d01a16e03ee1b81d5ad2e1ae
raghad <raghad.dwaik22@gmail.com>
2023-04-08 06:20:33 -0700

    hi

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -9,15 +10,18 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     private BottomNavigationView bottom;
     private ActionBarDrawerToggle toggle;
@@ -33,14 +37,14 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.place_recycler);
         final DrawerLayout drawerLayout = findViewById(R.id.DrawerLayout);
       //  final BottomNavigationView bottom=findViewById(R.id.bottom_nav);
-        navigationView=findViewById(R.id.navigation);
-     //  bottom=findViewById(R.id.bottom);
+      //  navigationView=findViewById(R.id.navigation);
+      bottom=findViewById(R.id.bottom);
 
       /*  toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.start,R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();*/
 
-
+      bottom.setOnNavigationItemSelectedListener(this);
 
 
         findViewById(R.id.menu).setOnClickListener(new View.OnClickListener(){
@@ -50,11 +54,11 @@ public class MainActivity extends AppCompatActivity {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
-        NavigationView nav =findViewById(R.id.navigation);
-        nav.setItemIconTintList(null);
+       NavigationView nav =findViewById(R.id.navigation);
+       nav.setItemIconTintList(null);
 
-       // BottomNavigationView nav1 =findViewById(R.id.bottom);
-        //nav1.setItemIconTintList(null);
+       BottomNavigationView nav1 =findViewById(R.id.bottom);
+        nav1.setItemIconTintList(null);
 
 
 
@@ -72,4 +76,26 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch(id){
+            case R.id.home:
+                Intent in = new Intent (this,MainActivity.class);
+                startActivity(in);
+                return true;
+            case R.id.map:
+                Intent in1 = new Intent (this,Map.class);
+                startActivity(in1);
+                return true;
+            case R.id.profile:
+                Intent in2 = new Intent (this,Profile.class);
+                startActivity(in2);
+                return true;
+
+        }
+        return false;
+
+    }
 }
